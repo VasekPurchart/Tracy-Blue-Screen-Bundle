5e102888e33d63128ff1016fe66a53be380e7bac
Vasek Purchart <me@vasekpurchart.cz>
2015-06-06 23:49:55 +0200

    configurable listener priority

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -7,12 +7,44 @@ use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 class Configuration implements \Symfony\Component\Config\Definition\ConfigurationInterface
 {
 
+	const PARAMETER_CONTROLLER_LISTENER_PRIORITY = 'listener_priority';
+
+	const SECTION_CONTROLLER = 'controller';
+
+	/** @var string */
+	private $rootNode;
+
+	/**
+	 * @param string $rootNode
+	 */
+	public function __construct($rootNode)
+	{
+		$this->rootNode = $rootNode;
+	}
+
 	/**
 	 * @return \Symfony\Component\Config\Definition\Builder\TreeBuilder
 	 */
 	public function getConfigTreeBuilder()
 	{
-		return new TreeBuilder();
+		$treeBuilder = new TreeBuilder();
+		$rootNode = $treeBuilder->root($this->rootNode);
+
+		$rootNode
+			->children()
+				->arrayNode(self::SECTION_CONTROLLER)
+					->addDefaultsIfNotSet()
+					->children()
+						->integerNode(self::PARAMETER_CONTROLLER_LISTENER_PRIORITY)
+							->info('Priority with which the listener will be registered.')
+							->defaultValue(0)
+							->end()
+						->end()
+					->end()
+				->end()
+			->end();
+
+		return $treeBuilder;
 	}
 
 }
--- src/DependencyInjection/TracyBlueScreenExtension.php
+++ src/DependencyInjection/TracyBlueScreenExtension.php
@@ -6,15 +6,22 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class TracyBlueScreenExtension extends \Symfony\Component\HttpKernel\DependencyInjection\Extension
+class TracyBlueScreenExtension extends \Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension
 {
 
+	const CONTAINER_PARAMETER_CONTROLLER_LISTENER_PRIORITY = 'vasek_purchart.tracy_blue_screen.controller.listener_priority';
+
 	/**
-	 * @param mixed[][] $configs
+	 * @param mixed[] $mergedConfig
 	 * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
 	 */
-	public function load(array $configs, ContainerBuilder $container)
+	public function loadInternal(array $mergedConfig, ContainerBuilder $container)
 	{
+		$container->setParameter(
+			self::CONTAINER_PARAMETER_CONTROLLER_LISTENER_PRIORITY,
+			$mergedConfig[Configuration::SECTION_CONTROLLER][Configuration::PARAMETER_CONTROLLER_LISTENER_PRIORITY]
+		);
+
 		$loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/config'));
 		$loader->load('services.yml');
 
@@ -24,6 +31,18 @@ class TracyBlueScreenExtension extends \Symfony\Component\HttpKernel\DependencyI
 		if ($environment === 'dev' && $debug === true) {
 			$loader->load('controller_listener.yml');
 		}
+	}
+
+	/**
+	 * @param mixed[] $config
+	 * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+	 * @return \VasekPurchart\TracyBlueScreenBundle\DependencyInjection\Configuration
+	 */
+	public function getConfiguration(array $config, ContainerBuilder $container)
+	{
+		return new Configuration(
+			$this->getAlias()
+		);
 	}
 
 }
